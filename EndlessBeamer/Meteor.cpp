ca8b7a17c159b5238a2ac64d521f4664d57d59b9
a <a>
2023-08-29 17:00:33 +0900

    Meteorクラス完了

--- EndlessBeamer/Meteor.cpp
+++ EndlessBeamer/Meteor.cpp
@@ -8,12 +8,15 @@ const float WaveSpeed = 0.5f;
 const float Speed = 40.f;
 const float WaveLength = StageWidth / 4.f;
 const float OneEighthRad = 0.25f;
+const int MainAlphaBlend = 200;
+
 
 Meteor::Meteor(VECTOR _pos, bool _waveFlag)
 {
 	pos = _pos;
 	upperPos = pos;
 	lowerPos = pos;
+	posMarker = pos;
 	firstPosX = _pos.x;
 	rad = 0.f;
 	waveRad = 0.f;
@@ -41,17 +44,26 @@ void Meteor::Update(float deltaTime)
 	{
 		deadFlag = true;
 	}
+	posMarker.y = MarkerPosY;
 
+	//íºï˚ëÃï`âÊópÇÃç¿ïWåvéZ
 	upperPos.x = pos.x + CubeSize * cos(rad * DX_PI_F);
 	lowerPos.x = pos.x - CubeSize * cos(rad * DX_PI_F);
 	upperPos.y = pos.y + CubeSize * cos(OneEighthRad * DX_PI_F);
 	lowerPos.y = pos.y - CubeSize * cos(OneEighthRad * DX_PI_F);
+	upperPos.z = pos.z + CubeSize * sin(rad * DX_PI_F);
+	lowerPos.z = pos.z - CubeSize * sin(rad * DX_PI_F);
 
 	ColorCanger();
 }
 
 void Meteor::Draw()
 {
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, MainAlphaBlend);
+	DrawCube3D(upperPos, lowerPos, color, color, true);
+	SetDrawBlendMode(DX_BLENDGRAPHTYPE_ALPHA, MaxAlphaRatio);
+	DrawCube3D(upperPos, lowerPos, color, color, false);
+	DrawLine3D(pos, posMarker, color);
 }
 
 void Meteor::AddRad(float deltaTime,float& _rad)
--- EndlessBeamer/Meteor.h
+++ EndlessBeamer/Meteor.h
@@ -13,8 +13,8 @@ private:
     float rad;//Meteorの回転角
     float waveRad;//ウェーブ制御用
     bool waveFlag;//ウェーブ移動するならtrue
-    VECTOR upperPos;//立方体の上のpos
-    VECTOR lowerPos;//立方体の下のpos
+    VECTOR upperPos;//直方体の上のpos
+    VECTOR lowerPos;//直方体の下のpos
     float firstPosX;//生成されたposのx座標
 
     void AddRad(float deltaTime, float& _rad);//ラジアン角の追加、2.0以上になったらリセット
